602db30075d7d5f91f4bf4750a2a0596bd42caa7
EricGabriel7 <ethegmc@gmail.com>
2019-06-17 17:02:43 -0400

    first commit

---
+++ src/IffyLoopy.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class IffyLoopy {
+    public static void main(String[] args){
+    Scanner keyboard = new Scanner(System.in);
+    String fiveValues;
+    fiveValues = keyboard.next();
+    boolean redEyes = false;
+
+    }
+}
